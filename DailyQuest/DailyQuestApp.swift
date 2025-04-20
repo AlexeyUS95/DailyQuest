9d27bcc78f19c0d730f80a800c87a4f22e38f22f
Alexey <alexey.usenko.ua@gmail.com>
2025-04-20 15:13:23 +0300

    Initial Commit

---
+++ DailyQuest/DailyQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuestApp.swift
+//  DailyQuest
+//
+//  Created by mac on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
